38ab28b8bc6ee922f06112a9b84a308c12a75f5e
figarotriana <figarotriana@gmail.com>
2025-04-22 00:10:36 -0500

    creation

---
+++ autoclicker_for_mc.py
@@ -0,0 +1,5 @@
+'''
+This is a practice script, I want to do an autoclicker
+I like to play minecraft but during work hours I tend to be AFK
+So the plan is to do a script that makes steve jump every 25 minutes to avoid getting disconnected for idleing
+'''
