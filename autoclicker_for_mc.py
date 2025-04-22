e44bca7a4b77597e0dea4206db6a3a10c3688979
figarotriana <figarotriana@gmail.com>
2025-04-22 14:40:24 -0500

    first version

--- autoclicker_for_mc.py
+++ autoclicker_for_mc.py
@@ -3,3 +3,13 @@ This is a practice script, I want to do an autoclicker
 I like to play minecraft but during work hours I tend to be AFK
 So the plan is to do a script that makes steve jump every 25 minutes to avoid getting disconnected for idleing
 '''
+import keyboard
+import time
+
+Wait_time = 2
+Hour = time.strftime("%H:%M:%S")
+
+while True:
+    time.sleep(Wait_time)
+    keyboard.press_and_release('space')
+    print ("space bar at " + time.strftime("%H:%M:%S"))                
