7a31efd98466d4b77a162228ebc9fae8faf397d7
figarotriana <figarotriana@gmail.com>
2025-04-22 15:12:07 -0500

    minecraft is not detecting it, I'm going to try with another library

--- autoclicker_for_mc.py
+++ autoclicker_for_mc.py
@@ -3,19 +3,13 @@ This is a practice script, I want to do an autoclicker
 I like to play minecraft but during work hours I tend to be AFK
 So the plan is to do a script that makes steve jump every 25 minutes to avoid getting disconnected for idleing
 '''
-import keyboard
 import time
+import pyautogui
 
-Wait_time = 2
-Hour = time.strftime("%H:%M:%S")
+def press_space():
+    pyautogui.press('space')  # Simula la pulsación de la barra espaciadora
 
 while True:
-    if keyboard.is_pressed('esc'):
-        print("STOPPED")
-        break
-    #so it doesn't repeat forever
-    
-    time.sleep(Wait_time)
-    keyboard.press_and_release('space')
-    print ("space bar at " + time.strftime("%H:%M:%S"))
-      
+    press_space()  # Simula la pulsación
+    print(f"Space bar pressed at {time.strftime('%H:%M:%S')}")
+    time.sleep(1)  
