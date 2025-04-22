b3749594e73effea3e7cc9e58359ce8e2fe751b2
figarotriana <figarotriana@gmail.com>
2025-04-22 15:38:38 -0500

    didn't worked inside of the game, going to try again later

--- autoclicker_for_mc.py
+++ autoclicker_for_mc.py
@@ -3,13 +3,17 @@ This is a practice script, I want to do an autoclicker
 I like to play minecraft but during work hours I tend to be AFK
 So the plan is to do a script that makes steve jump every 25 minutes to avoid getting disconnected for idleing
 '''
+
+'''
+proyect aborted, the script works everywhere except for minecraft
+I'm going to try again in the future
+'''
 import time
 import pyautogui
 
 def press_space():
-    pyautogui.press('space')  # Simula la pulsación de la barra espaciadora
-
+    pyautogui.press('space')  
 while True:
-    press_space()  # Simula la pulsación
-    print(f"Space bar pressed at {time.strftime('%H:%M:%S')}")
-    time.sleep(1)  
+    press_space()  
+    print(f"Space bar pressed at - {time.strftime('%H:%M:%S')}") 
+    time.sleep(1)      
