444cf8133ec5d0991a04357b0d940c533a9f3bea
figarotriana <figarotriana@gmail.com>
2025-04-29 16:52:06 -0500

    optimized confirm function, added filter for empty messages

--- message_count.py
+++ message_count.py
@@ -1,7 +1,19 @@
 import os
 import datetime
-os.system('cls' if os.name == 'nt' else 'clear') # cleaning terminal
+os.system('cls' if os.name == 'nt' else 'clear') # clearing terminal
 chat = list()
+
+def confirm(): #defining confirmation function
+     confirmation = input("\033[103m\033[90m\033[1m Are you sure? (y/n): \033[0m") 
+     if confirmation.upper() == "Y":
+        return True
+     elif confirmation.upper() == "N":
+        print("\033[103m\033[90m\033[1mCANCELLED\033[0m")
+        return False
+     else:
+        print("\033[103m\033[90m\033[1mINVALID INPUT\033[0m")
+        return False
+         
 while True:
     message = input("""\033[1m -----------------------------------------------------------------------------------
                    \033[0m  \033[93m MESSAGE COUNTER
@@ -10,26 +22,16 @@ while True:
                      \033[1m \033[91m  LOG \033[0m to show previous messages\033[0m
                      \033[1m \033[91m  CLEAR \033[0m to delete history\033[0m
                    	 \033[1mEnter new message: \033[0m""") # base message
-    Time = datetime.datetime.now()
-    Time = (Time.strftime("%H:%M:%S"))  # actual time for each message    
+    time_now = datetime.datetime.now()
+    time_now = (time_now.strftime("%H:%M:%S"))  # actual time for each message    
     if message.upper() == "EXIT":
-        exit_confirmation = input("\033[103m\033[90m\033[1m Are you sure? (y/n): \033[0m") # Clearing terminal
-        if exit_confirmation.upper() == "Y":
+        if confirm():
             print("\033[103m\033[90m\033[1mTHANKS FOR COMING!\033[0m")
             break
-        elif exit_confirmation.lower() == "n":
-            print("\033[103m\033[90m\033[1mNOT CLOSED\033[0m")
-        else:
-            print("\033[103m\033[90m\033[1mINVALID INPUT, ABORTING\033[0m")
-    elif message.upper() == "CLEAR":
-        clear_confirmation = input("\033[103m\033[90m\033[1m Are you sure? (y/n): \033[0m") # Clearing terminal
-        if clear_confirmation.upper() == "Y":
+    elif message.upper() == "CLEAR":  # clearing terminal
+        if confirm():
             chat.clear()
             print("\033[103m\033[90m\033[1mCLEARED\033[0m")
-        elif clear_confirmation.lower() == "n":
-            print("\033[103m\033[90m\033[1mNOT CLEARED\033[0m")
-        else:
-            print("\033[103m\033[90m\033[1mINVALID INPUT, ABORTING\033[0m")
     elif message.upper() == "COUNT":
         print(F"\033[103m\033[90m\033[1m******{len(chat)} MESSAGES SENT SO FAR******\033[0m") # count message
     elif message.upper() == "LOG":
@@ -39,6 +41,6 @@ while True:
             print("\033[103m\033[90m\033[1m==== CHAT LOG ====\033[0m") # show previous messages
             for message in chat:
                 print(message)
-    else:
-        chat.append(f"{Time} - {message}")  # adding message to the log with message time
+    elif message.strip():
+        chat.append(f"{time_now} - {message}")  # adding message to the log with message time
     
