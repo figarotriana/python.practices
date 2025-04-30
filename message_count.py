68e16f90c3756f12bd9c45c5ebccbd053b55cbb6
figarotriana <figarotriana@gmail.com>
2025-04-29 22:43:21 -0500

    search engine added, color format optimized, increased visual appeal for terminal messages

--- message_count.py
+++ message_count.py
@@ -2,45 +2,62 @@ import os
 import datetime
 os.system('cls' if os.name == 'nt' else 'clear') # clearing terminal
 chat = list()
-
-def confirm(): #defining confirmation function
-     confirmation = input("\033[103m\033[90m\033[1m Are you sure? (y/n): \033[0m") 
+c_reset = "\033[0m"
+c_yellow = "\033[93m"
+c_red = "\033[1m \033[91m"
+c_title = "\033[103m\033[90m\033[1m"
+c_danger = "\033[97m\033[101m\033[1m"
+# for copy and paste {c_reset} {c_red} {c_title} {c_yellow}
+def confirm(): #defining confirmation functio
+     confirmation = input(f"{c_danger}Are you sure? (y/n): {c_reset}") 
      if confirmation.upper() == "Y":
         return True
      elif confirmation.upper() == "N":
-        print("\033[103m\033[90m\033[1mCANCELLED\033[0m")
+        print(f"{c_title}-------------------------------------CANCELLED-------------------------------------{c_reset}")
         return False
      else:
-        print("\033[103m\033[90m\033[1mINVALID INPUT\033[0m")
+        print(f"{c_title}-----------------------------------INVALID INPUT-----------------------------------{c_reset}")
         return False
          
 while True:
-    message = input("""\033[1m -----------------------------------------------------------------------------------
-                   \033[0m  \033[93m MESSAGE COUNTER
-                     \033[1m \033[91m  EXIT \033[0m to close counter
-                     \033[1m \033[91m  COUNT \033[0m to count messages so far
-                     \033[1m \033[91m  LOG \033[0m to show previous messages\033[0m
-                     \033[1m \033[91m  CLEAR \033[0m to delete history\033[0m
-                   	 \033[1mEnter new message: \033[0m""") # base message
+    message = input(f"""-----------------------------------------------------------------------------------
+                     {c_yellow}\t MESSAGE COUNTER
+                     {c_red}  EXIT {c_reset} to close counter
+                     {c_red}  COUNT {c_reset} to count messages so far
+                     {c_red}  SEARCH {c_reset} to search inside Log
+                     {c_red}  LOG {c_reset} to show previous messages\033[0m
+                     {c_red}  CLEAR {c_reset} to delete history\033[0m
+                   	 \033[1mEnter new message: {c_reset}""") # base message
     time_now = datetime.datetime.now()
     time_now = (time_now.strftime("%H:%M:%S"))  # actual time for each message    
     if message.upper() == "EXIT":
         if confirm():
-            print("\033[103m\033[90m\033[1mTHANKS FOR COMING!\033[0m")
+            print(f"{c_title}--------------------------------THANKS FOR COMING!--------------------------------{c_reset}")
             break
     elif message.upper() == "CLEAR":  # clearing terminal
         if confirm():
             chat.clear()
-            print("\033[103m\033[90m\033[1mCLEARED\033[0m")
+            print(f"{c_title}CLEARED{c_reset}")
     elif message.upper() == "COUNT":
-        print(F"\033[103m\033[90m\033[1m******{len(chat)} MESSAGES SENT SO FAR******\033[0m") # count message
+        print(f"{c_title}******{len(chat)} MESSAGES SENT SO FAR******{c_reset}") # count message
     elif message.upper() == "LOG":
         if len(chat) == 0:
-            print("\033[103m\033[90m\033[1m==== CHAT LOG EMPTY ====\033[0m")
+            print(f"{c_title}================================== CHAT LOG EMPTY =================================={c_reset}")
         else:
-            print("\033[103m\033[90m\033[1m==== CHAT LOG ====\033[0m") # show previous messages
+            print(f"{c_title}===================================== CHAT LOG ====================================={c_reset}") # show previous messages
             for message in chat:
                 print(message)
+            print(f"{c_title}===================================================================================={c_reset}")
+    elif message.upper() == "SEARCH": #search engine
+        search = input("Enter words to search: ")
+        search_results = list(filter(lambda results: search.upper() in results.upper(), chat))
+        if len(search_results) == 0:
+            print(f"{c_title}================================= SEARCH LOG EMPTY ================================{c_reset}")
+        else:
+            print(f"{c_title}==================================== SEARCH LOG ==================================={c_reset}") # show previous messages
+            for search in search_results:
+                print(search)
+            print(f"{c_title}===================================================================================={c_reset}")
     elif message.strip():
         chat.append(f"{time_now} - {message}")  # adding message to the log with message time
     
